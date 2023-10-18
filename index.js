b00d806b32b9e50c8a169df307e725329823bd67
Ron Peterson <ron.peterson@familysearch.org>
2023-10-18 05:31:47 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+{
+    impress().init()
+    
+    // https://mermaid.js.org/config/theming.html
+    mermaid.initialize({
+        theme: 'forest',
+        themeVariables: {
+            fontSize: 45,
+        }
+    });
+}
